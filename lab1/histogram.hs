cc337c635420228cd1e6c879a0f28b02554c9aeb
Patrick Thill <kevin.patrick.thill@gmail.com>
2011-10-03 15:00:31 -0700

    added bar scaling and multiple file input

--- lab1/histogram.hs
+++ lab1/histogram.hs
@@ -10,10 +10,24 @@ infixl 9 |>
 (|>) :: a -> (a -> b) -> b
 (|>) r g = g r
 
+--splits a string on whitespace just like the standard function words,
+--but also strips punctionation from the beginning and end of the
+--words
+myWords :: String -> [String]
+myWords s = words s
+            |> L.map stripPunctuation
+            |> L.filter (\w -> length w > 0)
+              where
+                stripPunctuation s = reverse . loop . reverse $ loop s
+                loop [] = []
+                loop w@(l:ls)
+                  | not $ isAlpha l = loop ls
+                  | isAlpha l = w
+
 --reads a string and produces a map from each word to the number of
 --times that word appeared in the string
 makeHistogram :: String -> M.Map String Int
-makeHistogram s = words s |> loop M.empty
+makeHistogram s = myWords s |> loop M.empty
   where
     loop m [] = m
     loop m (x:xs)
@@ -22,14 +36,41 @@ makeHistogram s = words s |> loop M.empty
 
 --takes a map of words to counts and creates a histogram
 makePicture :: M.Map String Int -> String
-makePicture m =
-  M.toList m |> L.sortBy (comparing snd) |> L.map makeLine |> join "\n"
-    where
-      join sep = foldl (\t h -> h ++ sep ++ t) ""
-      makeLine (word, cnt) =
-        replicate (10 - length word) ' ' ++ word ++ ": " ++ replicate cnt '*'
+makePicture m = L.sortBy (comparing snd) l |> L.map (makeLine max_len max_cnt) |> join "\n"
+  where
+    l = M.toList m
+    max_len = length $ fst $ L.maximumBy (comparing $ length . fst) l
+    max_cnt = snd $ L.maximumBy (comparing snd) l
+    
+--concatenates the strings in the list, separated by sep
+join :: String -> [String] -> String
+join sep = foldl (\t h -> h ++ sep ++ t) ""
+        
+--makes a line for a single word and its count
+makeLine :: Int -> Int -> (String, Int) -> String
+makeLine i max (word, cnt)
+  | cnt <= 0 = error "count should never be less than 1"
+  | cnt > 0 = 
+    word ++ replicate numspaces ' ' ++ replicate (numhashes cnt max (79 - i)) '#'
+      where 
+        numspaces = i + 1 - length word
+        
+--finds the number of hashes necessary in a line for a word with cnt
+--appearances, given that the most frequest word appeared maxCnt times
+--and space is the maximum number of characters remaining on the line
+numhashes :: Int -> Int -> Int -> Int
+numhashes cnt maxCnt space
+  | maxCnt <= space = cnt
+  | cnt * space <= maxCnt = 1
+  | otherwise = round $ fromIntegral space * fromIntegral cnt / fromIntegral maxCnt
         
 main = do
-  (fpath:_) <- getArgs
-  s <- readFile fpath
+  paths <- getArgs
+  s <- loop paths ""
   map toLower s |> makeHistogram |> makePicture |> putStr
+    where
+      loop :: [String] -> String -> IO String
+      loop (p:paths) s = do
+        text <- readFile p
+        loop paths (s ++ "\n" ++ text)
+      loop [] s = return s
